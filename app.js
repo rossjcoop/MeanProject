c0a1a134efcb8b22b5eab5b463292298fb39b675
Ross Cooper <rossjamescooper@gmail.com>
2019-02-20 15:44:48 -0800

    Added a small comment

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ mongoose.connection.on('connected', () => {
     console.log("Connected to database "+config.database);
 })
 
+//On Error
 mongoose.connection.on('error', (err) => {
     console.log("Database error "+err);
 })
