4222d1f25d2218b261d5aec7440db209750da4cd
Ross Cooper <rossjamescooper@gmail.com>
2019-02-22 16:05:01 -0800

    Added todo component, looping through hard-coded todos for now

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodosComponent } from './components/todos/todos.component';
+import { TodoComponent } from './components/todo/todo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodosComponent
+    TodosComponent,
+    TodoComponent
   ],
   imports: [
     BrowserModule,
